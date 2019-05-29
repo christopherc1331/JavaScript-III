2556d39311057c6ea5fcdf295021a6d01298ddae
Chris Cruz <christopherc1331@gmail.com>
2019-05-28 18:20:15 -0700

    Finished answering 4 questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. In global scope, this refers to the object that it's in
+* 2. When a method is called the object that the function is binded to is what this is referring to
+* 3. When this is inside of a constructor method, this refers to an instance of the constructor method when it is called
+* 4. When an object's "this" reference is overridden by apply/call, then this refers to the overridden object inside of the call/apply parenthesis
 *
 * write out a code example of each explanation above
 */
